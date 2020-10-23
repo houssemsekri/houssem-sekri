4f70520e64357dcdabb7456eb9cb9dd4a84d3188
houssem <midnightrider865@gmail.com>
2020-10-23 22:44:14 +0100

    sayee

---
+++ build/precache-manifest.a1f1242e5177c70f951b8c170331b0fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ef4200940c2bd09cfa31bfc71a1a56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "000628d055998cb92420",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5fbf5661003f9b41e292",
+    "url": "/static/css/main.531ed9d0.chunk.css"
+  },
+  {
+    "revision": "000628d055998cb92420",
+    "url": "/static/js/2.5d53f9d9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.5d53f9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbf5661003f9b41e292",
+    "url": "/static/js/main.ca5c3cb7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "850359e9c0619e34c94d8c2312f4f027",
+    "url": "/static/media/h.850359e9.png"
+  },
+  {
+    "revision": "3bfdf9cb2f6368bf65d4a1b1c321fdee",
+    "url": "/static/media/user.3bfdf9cb.jpg"
+  }
+]);
